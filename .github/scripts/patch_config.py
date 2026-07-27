66861672094c2bfdd2d1039f99690a9fc82df214
yydfys <540734621@qq.com>
2026-07-27 18:43:20 +0800

    fix: remove duplicate os import in patch

--- .github/scripts/patch_config.py
+++ .github/scripts/patch_config.py
@@ -1,12 +1,8 @@
-import os, sys
+import sys
 content = open('config/config.go').read()
 content = content.replace(
     'var globalConfig = &Config{\n\tDataPath: "/m3u-proxy/data/",\n\tTempPath: "/tmp/m3u-proxy/",\n}',
     'var globalConfig = &Config{\n\tDataPath: "/m3u-proxy/data/",\n\tTempPath: "/tmp/m3u-proxy/",\n}\nfunc init() {\n\tif d := os.Getenv("DATA_DIR"); d != "" {\n\t\tglobalConfig.DataPath = d\n\t}\n\tif t := os.Getenv("TEMP_DIR"); t != "" {\n\t\tglobalConfig.TempPath = t\n\t}\n}'
-)
-content = content.replace(
-    '"fmt"',
-    '"fmt"\n\t"os"'
 )
 open('config/config.go', 'w').write(content)
 print("patch OK")
